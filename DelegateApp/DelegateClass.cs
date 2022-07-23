49bcccaee29decfa867bd5ba4641765ee5a44785
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-07-23 06:20:08 +0100

    Add project files.

---
+++ DelegateApp/DelegateClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateApp
+{
+    public class DelegateClass
+    {
+        // declare a delegate method
+        public delegate void Calculate(int n,int k);
+        
+        public static void AddNumbers(int n,int k)
+        {
+            Console.WriteLine(n + k);
+        }
+        
+        public static void Substract(int n,int k)
+        {
+            Console.WriteLine(n - k);
+        }
+
+        public static void Multiply(int n, int k)
+        {
+            Console.WriteLine(n * k);
+        }
+
+        public static void Divide(int n, int k)
+        {
+            Console.WriteLine(n / k);
+        }
+    }
+}
---
+++ DelegateApp/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static DelegateApp.DelegateClass;
+
+//Instanting a delegate
+Calculate calculator;
+Calculate addCalculator = new Calculate(AddNumbers);
+Calculate substractCalculator = new Calculate(Substract);
+Calculate multiplyCalculator = new Calculate(Multiply);
+Calculate divideCalculator = new Calculate(Divide);
+
+//Calling the delegate instances at a time
+addCalculator(10, 5); // 15
+substractCalculator(10, 5); //5
+multiplyCalculator(10, 5); //50
+divideCalculator(10, 5); //2
+
+//multicasting
+//Calling the delagate method all at once
+calculator = addCalculator; // add two variables
+calculator += substractCalculator; // substract two variables
+calculator += multiplyCalculator;
+calculator += divideCalculator;
+calculator(10, 5); // 15 5 50 2
+
