13f5144ade34584c9601ebdb0019753aed38594d
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-08-04 10:48:33 +0100

    updated

--- DelegateApp/Program.cs
+++ DelegateApp/Program.cs
@@ -1,18 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DelegateApp;
 using static DelegateApp.DelegateClass;
+
 
 //Instanting a delegate
 Calculate calculator;
-Calculate addCalculator = new Calculate(AddNumbers);
-Calculate substractCalculator = new Calculate(Substract);
-Calculate multiplyCalculator = new Calculate(Multiply);
-Calculate divideCalculator = new Calculate(Divide);
+Calculate addCalculator = new(AddNumbers);
+Calculate substractCalculator = new(Substract);
+Calculate multiplyCalculator = new(Multiply);
+Calculate divideCalculator = new(Divide);
 
 //Calling the delegate instances at a time
 addCalculator(10, 5); // 15
 substractCalculator(10, 5); //5
 multiplyCalculator(10, 5); //50
 divideCalculator(10, 5); //2
+
 
 //multicasting
 //Calling the delagate method all at once
@@ -21,4 +24,5 @@ calculator += substractCalculator; // substract two variables
 calculator += multiplyCalculator;
 calculator += divideCalculator;
 calculator(10, 5); // 15 5 50 2
+
 
